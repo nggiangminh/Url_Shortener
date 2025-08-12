2a426688dec67865e08a9287b15839586b95e635
nguyen giang minh <nguyengiangminh99@gmail.com>
2025-08-12 21:53:51 +0700

    create project

---
+++ url-shortener-be/src/main/java/com/project/url_shortener_be/UrlShortenerBeApplication.java
@@ -0,0 +1,13 @@
+package com.project.url_shortener_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerBeApplication.class, args);
+	}
+
+}
---
+++ url-shortener-be/src/test/java/com/project/url_shortener_be/UrlShortenerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.url_shortener_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
